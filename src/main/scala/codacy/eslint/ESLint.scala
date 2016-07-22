9d74b65a6d4cb19deb4cd002d961445a32d81eee
Johann Egger <johann@codacy.com>
2016-07-22 18:17:47 +0300

    Bump the seed version

--- src/main/scala/codacy/eslint/ESLint.scala
+++ src/main/scala/codacy/eslint/ESLint.scala
@@ -39,11 +39,12 @@ object ESLint extends Tool {
           case _ => List.empty[String]
         }
 
-      val command = List("eslint", "--no-eslintrc",
-        "--plugin", "react",
-        "--plugin", "angular",
-        "-f", "checkstyle",
-        "-o", s"${outputFile.toFile.getCanonicalPath}") ++ configuration ++ filesToLint
+      val command = List("eslint") ++
+        (if (conf.nonEmpty) Some("--no-eslintrc") else None) ++
+        List("--plugin", "react",
+          "--plugin", "angular",
+          "-f", "checkstyle",
+          "-o", s"${outputFile.toFile.getCanonicalPath}") ++ configuration ++ filesToLint
 
       CommandRunner.exec(command) match {
         case Right(resultFromTool) =>
