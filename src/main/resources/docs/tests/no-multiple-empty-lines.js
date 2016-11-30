5832f36ab6b90ba5c5df30ee8658da228c7341c1
Sébastien Elet <sebastien@elet.fr>
2016-11-30 07:41:57 +0100

    test(no-multiple-empty-lines): update fixture for eslint 3.11.1

--- src/main/resources/docs/tests/no-multiple-empty-lines.js
+++ src/main/resources/docs/tests/no-multiple-empty-lines.js
@@ -1,7 +1,7 @@
 //#Patterns: no-multiple-empty-lines
-//#Issue: {"severity": "Info", "line": 6, "patternId": "no-multiple-empty-lines"}
+//#Issue: {"severity": "Info", "line": 4, "patternId": "no-multiple-empty-lines"}
 var foo = 5;
 
-    
-       
+
+
 var bar = 3;
