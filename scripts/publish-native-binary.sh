castelo1998/codacy-eslint

Test plugins-test binary

scripts/publish-native-binary.sh
@@ -106,7 +106,7 @@ case "$TARGET" in
       --entrypoint=bash \
       -v $HOME/.ivy2:$HOME/.ivy2 \
       -v $PWD:$PWD \
-      findepi/graalvm:1.0.0-rc2-all \
+      findepi/graalvm:1.0.0-rc3-all \
         -c 'cd /tmp && '"${BUILD_CMD}"' && mv '"$BINARY_NAME $PWD/build/$BINARY_NAME"
     ;;
   *)

archived-codacy/codacy-eslint-legacy

scripts/publish-native-binary.sh
@@ -106,7 +106,7 @@ case "$TARGET" in
       --entrypoint=bash \
       -v $HOME/.ivy2:$HOME/.ivy2 \
       -v $PWD:$PWD \
-      findepi/graalvm:1.0.0-rc2-all \
+      findepi/graalvm:1.0.0-rc4-all \
         -c 'cd /tmp && '"${BUILD_CMD}"' && mv '"$BINARY_NAME $PWD/build/$BINARY_NAME"
     ;;
   *)